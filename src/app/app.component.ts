3f647c4415e6636db29887f1b423290c2b498412
Carlos eduardo <carlos.contato00@gmail.com>
2024-02-01 19:56:22 -0300

    Insert roles aditional to-do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,50 @@ export class AppComponent {
 
   public title = 'to-do-list';
 
-  public list: string[] = [];
+  public list: {id: number, description: string}[] = [];
 
-  public constructor() {
-    this.list.push('Teste');
+  public field: string = '';
+
+  public constructor() {}
+
+  public observableField(e: any): void {
+    this.field = e.target.value;
   }
 
-  public set(description: string): void {
-    if (description.length < 1) return;
-    this.list.push(description);
+  public set(value: string): void {
+    if (value.length < 1) return;
+    if (this.isDuplicated(value)) {
+      alert('Tarefa já registrada!');
+      return;
+    }
+    this.list.push({ id: this.list.length + 1, description: value });
+    this.clearField();
+  }
+
+  public isDuplicated(description: string): boolean {
+    return this.list.filter((item) => item.description.toUpperCase() === description.toUpperCase()).length > 0 ?
+      true : false;
+  }
+
+  public delete(id: number): void {
+    const item = this.searchItem(id);
+    if (item.length > 0) {
+      const index = this.list.indexOf(item[0]);
+      if (index > -1) this.list.splice(index, 1);
+    }
+
+    if (this.list.length === 0 && this.field.length > 0) {
+      this.clearField();
+    }
+  }
+
+  public searchItem(id: number): {id: number, description: string}[] | [] {
+    if (this.list.length < 1) return [];
+    return this.list.filter((item) => item.id === id);
+  }
+
+  public clearField(): void {
     this.inputRef.nativeElement.value = '';
+    this.field = '';
   }
 }
